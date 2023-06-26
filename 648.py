ab4d98209714a7a874f429709fa6ed98893995b2
Ryland Hu <43745623+Catttttttt@users.noreply.github.com>
2023-06-26 15:23:48 +0800

    Update 648.py

--- 648.py
+++ 648.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type sentence: str
         :rtype: str
         """
+        """
         dictset = set(dictionary)
         words = sentence.split(" ")
         for index, word in enumerate(words):
@@ -12,4 +13,25 @@ class Solution(object):
                 if word[:j] in dictset:
                     words[index] = word[:j]
                     break
+        return " ".join(words)
+        """
+        trie = {}
+        for root in dictionary:
+            cur = trie
+            for c in root:
+                if c not in cur:
+                    cur[c] = {}
+                cur = cur[c]
+            cur[0] = {}
+        
+        words = sentence.split()
+        for index, word in enumerate(words):
+            cur = trie
+            for j, c in enumerate(word):
+                if 0 in cur:
+                    words[index] = word[:j]
+                    break
+                if c not in cur:
+                    break
+                cur = cur[c]
         return " ".join(words)
